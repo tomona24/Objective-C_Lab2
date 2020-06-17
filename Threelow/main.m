b3b525f84fb84f34d8c56d3f29943d87b7b4682a
Tomona Sako <sakotomona@gmail.com>
2020-06-16 21:34:03 -0700

    Started Threelow

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Tomona Sako on 2020/06/16.
+//  Copyright © 2020 Tomona Sako. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
